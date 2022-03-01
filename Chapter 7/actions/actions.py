a20106760356a79012e5686fd2364dbcbc733c32
tehmeerali786 <freelanceali786@gmail.com>
2022-03-01 13:37:26 -0600

    Chapter 7 without Form

--- Chapter 7/actions/actions.py
+++ Chapter 7/actions/actions.py
@@ -1,27 +1,100 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Tracker, Action
+from rasa_sdk.events import SlotSet 
+from rasa_sdk.executor import CollectingDispatcher 
+from rasa_sdk.forms import FormAction
+
+class TicketFormAction(FormAction):
+    def name(self) -> Text:
+        return "action_ticket_form_submit"
+
+    def run(
+        self, dispatch: CollectingDispatcher, tracker: Tracker, 
+        domain: Dict[Text, Any]
+    ) -> List[Dict]:
+        # print out all entities
+        dispatch.utter_message("Entities: " + tracker.latest_message['entities'] + "\n" + "Slots: " + tracker.slots)
+        return []
+
+class TicketQueryAction(Action):
+    def name(self) -> Text:
+        return "action_ticket_response"
+
+    def run(self, dispatch, tracker, domain):
+        entities = []
+        for entity in tracker.latest_message['entities']:
+            entities.append(
+                {"entity": entity["entity"], "value": entity["value"],
+                 "role": entity.get("role")})
+
+        msg = str(entities) + "\n"
+
+        dispatch.utter_message(msg)
+        return []
+
+class DrinkQueryAction(Action):
+    def name(self) -> Text: 
+        return "action_ticket_response"
+
+    def run(self, dispatch, tracker, domain):
+        # Print out entities by group
+        msg = ""
+
+        entities = []
+
+        for entity in tracker.latest_message['entities']:
+            if entity.get("group") == "1":
+                entities.append(
+                    {"entity": entity["entity"], "value": entity["value"], "group": entity["group"]})
+                msg = msg + "group #1: " + str(entities) + "\n"
+
+            elif entity.get("group") == "2":
+                entities.append(
+                    {"entity": entity["entity"], "value": entity["value"], "group": entity["group"]})
+                msg = msg + "group #2: " + str(entities) + "\n"
+
+            elif entity.get("group") == "3":
+                entities.append(
+                    {"entity": entity["entity"], "value": entity["value"], "group": entity["group"]})
+                msg = msg + "group #3: " + str(entities) + "\n"
+
+            elif entity.get("group") == "4":
+                entities.append(
+                    {"entity": entity["entity"], "value": entity["value"], "group": entity["group"]})
+                msg = msg + "group #4: " + str(entities) + "\n"
+
+            elif entity.get("group") == "5":
+                entities.append(
+                    {"entity": entity["entity"], "value": entity["value"], "group": entity["group"]})
+                msg = msg + "group #5: " + str(entities) + "\n"
+
+            elif entity.get("group") == "6":
+                entities.append(
+                    {"entity": entity["entity"], "value": entity["value"], "group": entity["group"]})
+                msg = msg + "group #6: " + str(entities) + "\n"
+
+            else:
+                entities = ["Not Found"]
+                msg = msg + str(entities) + "\n"
+
+        dispatch.utter_message(msg)
+        return []
+
+class ActionBuyTicket(Action):
+    def name(self) -> Text:
+        return "action_buy_ticket"
+
+    def run(
+        self, 
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        arrive = tracker.get_slot("city_arrive")
+
+        api_succeed = arrive == "Moscow"
+
+        return [SlotSet("api_succeed", api_succeed)]
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
